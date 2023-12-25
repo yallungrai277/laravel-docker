345f180ba1a06b56f410fe6f65340de8e7500668
Sudip Rai <sudiprai@Sudips-MacBook-Pro-2.local>
2023-12-25 19:43:34 +1100

    Test

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application name
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application. This value is used when the
